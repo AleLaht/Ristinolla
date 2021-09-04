f1d4551be3549a051f74a5c8e27e4536d6d09be2
alelaht <lahtinen.ale@gmail.com>
2021-09-04 14:48:48 +0300

    Handleclicks palikkalisätty

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 let gameActive = true;
-let currentPlayer = 'X';
+let currentPlayer = 'O';
 let tiles = Array.from(document.querySelectorAll('.tile'));
+let board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+]
+
 
 const gameboard = {
     tiles: Array.from(document.querySelectorAll('.tile')),
@@ -17,15 +23,27 @@ let tile1 = tiles[0];
 let changeCurrentPlayer = () => {
     if (currentPlayer === 'X') {
         currentPlayer = 'O';
-    } else {
+    } else if (currentPlayer === 'O') {
         currentPlayer = 'X';
+    } else {
+        return 'Error!'
     }
 }
 
-// handle clicks on gameboard tiles
-tile1.onclick = function(handleClick) {
-    document.getElementById('asd').innerHTML = currentPlayer;
-    changeCurrentPlayer();
-};
 
-console.log(gameboard.tiles[5]);
+
+// handle clicks on gameboard tiles
+
+for (let i = 0; i < tiles.length; i++) {
+    tiles[i].onclick = function(handleClick) {
+        tiles.forEach((el) => {
+            board[el[0]] = el[1];
+        });
+        this.innerHTML = currentPlayer
+        changeCurrentPlayer();
+    };
+}
+
+console.log(gameboard.tiles);
+console.log(board)
+
